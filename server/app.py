513b8af3ae27caf13de2572e50a48b6d675c7503
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-09-27 17:37:37 +0300

    assignment submission

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
+
+
 from models import db, Message
 
 app = Flask(__name__)
@@ -12,7 +14,12 @@ app.json.compact = False
 CORS(app)
 migrate = Migrate(app, db)
 
+
 db.init_app(app)
+
+
+
+
 
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
